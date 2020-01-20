11134a643a8c2118e764aceadac995aa5ed75804
graphicstone <harishiv8@gmail.com>
2020-01-20 16:34:05 +0530

    Random array generated using the algorithm

--- app/src/main/java/com/example/oneside/MainActivity.kt
+++ app/src/main/java/com/example/oneside/MainActivity.kt
@@ -2,41 +2,55 @@ package com.example.oneside
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val randomArray = ArrayList<Int>()
+    private val fixedArray = ArrayList<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if(supportActionBar != null)
+        if (supportActionBar != null)
             supportActionBar?.hide()
 
-        val randomNo = ArrayList<Int>()
-        val fixedNo = ArrayList<Int>()
+        for (i in 1..9) {
+            randomArray.add(i)
+        }
+        for (i in 1..9) {
+            fixedArray.add(i)
+        }
 
-        randomNo.add(1)
-        randomNo.add(3)
-        randomNo.add(2)
-        randomNo.add(6)
-        randomNo.add(7)
-        randomNo.add(9)
-        randomNo.add(4)
-        randomNo.add(8)
-        randomNo.add(5)
+        val outerLoopRandomNo = (4..7).random()
+        for (i in 0..outerLoopRandomNo)
+            for (j in 0..2)
+                for (k in 0..2) {
+                    val swapSite = (0..3).random()
+                    val swapDirection = (0..1).random()
+                    rotateGrid(swapSite, swapDirection)
+                }
 
-        fixedNo.add(1)
-        fixedNo.add(2)
-        fixedNo.add(3)
-        fixedNo.add(4)
-        fixedNo.add(5)
-        fixedNo.add(6)
-        fixedNo.add(7)
-        fixedNo.add(8)
-        fixedNo.add(9)
-
-        val adapter = GridViewAdapter(this@MainActivity, fixedNo, randomNo)
+        val adapter = GridViewAdapter(this@MainActivity, fixedArray, randomArray)
         gl_matrix_layout.adapter = adapter
+    }
+
+    private fun rotateGrid(swapSite: Int, swapDirection: Int) {
+        if (swapDirection == 0) {
+            swapNumbers(swapSite, swapSite + 3)
+            swapNumbers(swapSite + 4, swapSite + 1)
+        } else {
+            swapNumbers(swapSite, swapSite + 1)
+            swapNumbers(swapSite + 4, swapSite + 3)
+        }
+    }
+
+    private fun swapNumbers(i: Int, j: Int) {
+        randomArray[i]=randomArray[j].also { randomArray[j]  = randomArray[i]}
+        Log.i("Position1:", randomArray[0].toString()+randomArray[1].toString()+randomArray[2])
+        Log.i("Position2:", randomArray[3].toString()+randomArray[4].toString()+randomArray[5])
+        Log.i("Position3:", randomArray[6].toString()+randomArray[7].toString()+randomArray[8])
     }
 }
